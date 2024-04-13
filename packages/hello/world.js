c2b8179efbad80cc4a8b7227121b159f663b73ed
Michele Sciabarra <30654959+sciabarracom@users.noreply.github.com>
2024-04-13 12:50:56 +0100

    fix to variables

--- packages/hello/world.js
+++ packages/hello/world.js
@@ -1,9 +1,9 @@
 //--web true
-//--param DEFAULT_MESSAGE $DEFAULT_MESSAGE
+//--param DEFAULT_MESSAGE "$DEFAULT_MESSAGE"
 function main(args) {
     let message = args.name || args.DEFAULT_MESSAGE || "stranger"
     console.log(message)
     return {
-        "body": `<h1>Hello, ${message}.</h1>`
+        "body": `<h1>Hello, ${message}...</h1>`
     }
 }
