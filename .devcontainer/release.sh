3fb2a7763a2177a664d5103d59ff405801232691
Michele Sciabarra <michele@nuvolaris.io>
2024-06-01 21:39:43 +0200

    starter

--- .devcontainer/release.sh
+++ .devcontainer/release.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 VER=v$(date +%y%m%d%H%M)
-IMG=ghcr.io/mastrogpt/starter
+IMG=ghcr.io/sciabarracom/starter
 docker build . -t $IMG:$VER --push
 sed -i -e "s!\"image\": \".*\"!\"image\": \"$IMG:$VER\"!" devcontainer.json
  
