0b8cb875584ae900162e02a20d6eaeacdba5c542
Michele Sciabarra <michele@nuvolaris.io>
2024-06-02 03:27:26 +0200

    v2406020324

--- .devcontainer/release.sh
+++ .devcontainer/release.sh
@@ -7,6 +7,9 @@ if [[ $URL =~ (https?://github\.com/|git@github\.com:)([^/]+/[^/]+) ]]; then
     IMG=ghcr.io/"${BASH_REMATCH[2]}"
     docker build . -t $IMG:$VER --push
     sed -i -e "s!\"image\": \".*\"!\"image\": \"$IMG:$VER\"!" devcontainer.json
+    git add devcontainer.json
+    git commit -m "$VER" -a
+    git push origin main
 else
     echo "Invalid or unsupported URL format in git remote origin"
 fi
